f6f330a16600bf1a67916aade5b098245b436466
ataberkakkan <akkanataberk@gmail.com>
2023-11-04 16:50:26 +0300

    Redux config Login and Register pages

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 
-
-export default function Navbar() {
+const Navbar = () => {
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -11,7 +10,6 @@ export default function Navbar() {
           </h1>
         </Link>
 
-       
         <ul className="flex items-center gap-4">
           <Link to="/">
             <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
@@ -24,15 +22,16 @@ export default function Navbar() {
             </li>
           </Link>
 
-            <Link to="/sign-in">
-              <li className="text-slate-700 hover:underline sm:cursor-pointer">
-                {""}
-                Sign in
-              </li>
-            </Link>
-       
+          <Link to="/sign-in">
+            <li className="text-slate-700 hover:underline sm:cursor-pointer">
+              {""}
+              Sign in
+            </li>
+          </Link>
         </ul>
       </div>
     </header>
   );
-}
+};
+
+export default Navbar;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./app/store.js";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+//Pages
+import Home from "./pages/Home.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import About from "./pages/About.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
