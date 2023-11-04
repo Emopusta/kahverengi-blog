a94cff870d5e0a9678abe4cf612502702bbc7744
Emopusta <emreduman225@hotmail.com>
2023-11-04 21:53:18 +0300

    dynamic port handling in vite.config and endpoint url constants

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv  } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  
+  return {
+      
+      plugins: [react()],
+      server: {
+        proxy: {
+          "/api": {
+            target: `http://localhost:${(env.VITE_PORT ? env.VITE_PORT : 8000).toString()}`,
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ""),
+          },
+        },
       },
-    },
-  },
+    
+  }
 });
