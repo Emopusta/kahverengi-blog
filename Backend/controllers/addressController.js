6340aecfa8f5f4ac2ff921ebfcd07fb19d32791e
ataberkakkan <akkanataberk@gmail.com>
2023-11-23 12:10:31 +0300

    Changed the address model and controller for fix

--- Backend/controllers/addressController.js
+++ Backend/controllers/addressController.js
@@ -1,12 +1,16 @@
 import asyncHandler from "../middleware/asyncHandler.js";
-import Address from "../models/addressModel.js"
+import Address from "../models/addressModel.js";
 
 const create = asyncHandler(async (req, res) => {
   const { country, city, county } = req.body;
-  const addressInputs = { country, city, county };
-  const address = await Address.create(addressInputs);
+
+  const address = await Address.create({
+    country,
+    city,
+    county,
+  });
+
   return res.status(201).json(address);
 });
-
 
 export { create };
